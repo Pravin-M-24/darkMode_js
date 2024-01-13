b2fbca2f0fd80cce38066227d2d40ce439bede42
Your Name <you@example.com>
2024-01-14 00:18:31 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let togglediv = document.getElementById("toggle_container");
-let bodytag = document.getElementsByTagName("body");
-let h1 = document.getElementsByTagName("h1");
+let bodytag = document.getElementsByTagName("body")[0];
+let circle = document.getElementById("circle");
+console.log(bodytag);
+let h1 = document.getElementsByTagName("h1")[0];
 let toggle = false;
 function changeMode(){
      if(!toggle){
         bodytag.classList.add("bgclr");
+        h1.classList.add("h1_darkmode");
+        circle.classList.add("dark-mode");
         toggle = true;
      }
      else{
         bodytag.classList.remove("bgclr");
+        h1.classList.remove("h1_darkmode");
+        circle.classList.remove("dark-mode");
         toggle = false;
      }
 }
-
 togglediv.addEventListener("click" , changeMode);
